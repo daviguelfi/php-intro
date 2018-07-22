719cc5c02c006d606e90de2195f2006c735bbc4b
daviguelfi <33698595+daviguelfi@users.noreply.github.com>
2018-07-22 14:01:24 -0300

    Add files via upload
    Adicionando validação de campos do formulário

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="utf-8">
+    <title>Formulário de Inscrição</title>
+<head>
+<body>
+
+<p>FORMULÁRIO PARA INSCRIÇÃO DE COMPETEDIDORES</p>
+
+    <form action="script.php" method="get">
+        <p>Seu nome: <input type="text" name="nome"/></p>
+        <p>Sua idade: <input type="text" name="idade"/></p>
+        <p><input type="submit" value="Enviar dados do competidor"/></p>
+    </form>
+
+</body>
+</html>    
