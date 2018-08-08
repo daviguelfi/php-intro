ba40ea904d2bcf42872789764eb767bc197741eb
daviguelfi <33698595+daviguelfi@users.noreply.github.com>
2018-08-08 09:33:04 -0300

    Add files via upload
    Criacao de servicos e funcoes php

--- script.php
+++ script.php
@@ -1,41 +1,14 @@
 <?php
 
-session_start ();
-
-$categorias = [];
-$categorias[] = 'infantil';
-$categorias[] = 'adolescente';
-$categorias[] = 'adulto';
+include "servicos/servicoMensagemSessao.php";
+include "servicos/servicoValidacao.php";
+include "servicos/servicoCategoriaCompetidor.php";
 
 $nome = $_POST['nome']; 
 $idade = $_POST['idade'];
 
+defineCategoriaCompetidor($nome, $idade);
 
+header(string 'location: index.php');
 
-
-if($idade >= 6 && $idade <= 12){
-    for($i = 0; $i <= count($categorias); $i++){
-        if ($categorias[$i] == 'infantil'){
-            $_SESSION['mensagem-de-sucesso'] =  "O nadador " .$nome. " compete na categoria " .$categorias[$i];
-            header(string: 'location: index.php');
-            return;
-        }
-    }
-}
-else if($idade >=13 && $idade <=18){
-    for($i = 0; $i <= count($categorias); $i++){
-        if($categorias[$i] == 'adolescente'){
-            $_SESSION['mensagem-de-sucesso'] =  "O nadador " .$nome. " compete na categoria " .$categorias[$i];
-            header(string: 'location: index.php');
-            return;
-        }
-    }
-}
-else {
-    for($i = 0; $i <= count($categorias); $i++){
-        if($categorias[$i] == 'adulto'){
-            echo  "O nadador " .$nome. " compete na categoria adulto";
-        }
-    }
-}
 
